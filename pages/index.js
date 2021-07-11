3a1ad97bb2fcc63bb0a8566b2ece5ca1223e1083
Felipe <felipedantas184@gmail.com>
2021-07-11 15:24:16 -0300

    come on Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import Footer from '../components/Footer'
-import Navbar from '../components/NavBar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
